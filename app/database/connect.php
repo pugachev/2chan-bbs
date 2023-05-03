857da4053b9612414c84a7b04f9a2a243cddb79c
pugachev <pugachev2011@gmail.com>
2023-05-03 11:04:17 +0900

    DB接続情報を修正

--- app/database/connect.php
+++ app/database/connect.php
@@ -5,7 +5,7 @@ $pass = "Manabu2010";
 
 //DBと接続
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=2chan-bbs', $user, $pass);
+    $pdo = new PDO('mysql:host=localhost;dbname=ikefuku40_2chanbbs', $user, $pass);
     // echo "DBとの接続に成功しました";
 } catch (PDOException $error) {
     echo $error->getMessage();
